af37c6c4a1a1aba5a4fa03438224c508445104d2
Maja <maja@localhost>
2021-04-11 21:12:23 +0300

    есть кнопки, но только картинки

--- TowerDefense.py
+++ TowerDefense.py
@@ -1,6 +1,7 @@
 from enemyTD import *
 from towerTD import *
 from utils import *
+from Buttons import *
 
 pygame.init()
 screen = pygame.display.set_mode((1200, 900))
@@ -8,9 +9,6 @@ screen = pygame.display.set_mode((1200, 900))
 bg = load_img(f"images/Background/Tower_Defense_fon.png")
 bg = pygame.transform.scale(bg, (1200, 900))
 
-
-#snow_bat = Enemy(snow_bat_imgs, snow_bat_die_imgs, snow_bat_hurt_imgs, path)
-#skull_troll = Enemy(skull_troll_imgs, skull_troll_die_imgs, skull_troll_hurt_imgs, path)
 waves_enemies = [[snow_bat_imgs, snow_bat_die_imgs, snow_bat_hurt_imgs, 15],
                  [skull_troll_imgs, skull_troll_die_imgs, skull_troll_hurt_imgs, 20],
                  [troll_bat_imgs, troll_bat_die_imgs, troll_bat_hurt_imgs, 30]]
@@ -19,21 +17,24 @@ enemies = []
 stones = []
 places = [[520, 303], [773, 303], [1022, 301], [437, 547], [711, 558], [975, 564], [304, 758]]
 enemy_gen = EnemyGen(waves_enemies)
+menu = GameMenu()
 while True:
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             exit()
 
-        if e.type == pygame.MOUSEBUTTONDOWN:
-            pos = pygame.mouse.get_pos()
-            pos = list(pos)
-            #print(pos)
-            pos[0] -= 30
-            pos[1] -= 60
-            stone_tower = StoneTower(pos[0], pos[1], tower_imgs)
-            stone_towers.append(stone_tower)
+        # if e.type == pygame.MOUSEBUTTONDOWN:
+        #     pos = pygame.mouse.get_pos()
+        #     pos = list(pos)
+        #     #print(pos)
+        #     pos[0] -= 30
+        #     pos[1] -= 60
+        #     stone_tower = StoneTower(pos[0], pos[1], tower_imgs)
+        #     stone_towers.append(stone_tower)
 
     screen.blit(bg, (0, 0))
+
+    menu.draw(screen)
 
     enemy_gen.update()
     new_enemy = enemy_gen.new_enemy()
@@ -49,7 +50,11 @@ while True:
             e.move()
 
     for j in places:
-        pygame.draw.circle(screen, (255, 20, 147), (j[0], j[1]), 10)
+        radius = 50
+        x, y = j[0] - 20, j[1] - 25
+        surface = pygame.Surface((radius * 2, radius * 2), pygame.SRCALPHA)
+        pygame.draw.circle(surface, (0, 255, 0, 100), (radius, radius), radius, 0)
+        screen.blit(surface, (x, y))
 
     for stone_tower in stone_towers:
         stone_tower.draw(screen)
